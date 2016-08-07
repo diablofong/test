14285cdbf2bfd37289596eb24e75a45991734be5
diablofong <diablofong@gmail.com>
2016-08-07 23:33:51 +0800

    change port

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ var express = require("express");
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World999!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(8000, function () {
+  console.log('Example app listening on port 8000!');
 });
